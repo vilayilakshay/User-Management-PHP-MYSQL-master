97f2cd7330eca90bdeaa9859ac889c8135ad5bbe
vilayilakshay <42640063+vilayilakshay@users.noreply.github.com>
2020-07-31 14:14:40 -0700

    update

--- includes/config.php
+++ includes/config.php
@@ -1,13 +1,14 @@
 ﻿<?php 
 // DB credentials.
-define('DB_HOST','fdb15.eohost.com');
-define('DB_USER','2520448_armentum');
-define('DB_PASS','963852asd');
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
 define('DB_NAME','2520448_armentum');
 // Establish database connection.
 try
 {
 $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
+print_r("Ajay");
 }
 catch (PDOException $e)
 {
